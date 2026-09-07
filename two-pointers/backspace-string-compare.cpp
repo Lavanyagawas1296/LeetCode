9c928109e1632d98b661c54126ebaf883d4e55f4
Lavanya Gawas <lavanyagawas07@gmail.com>
2026-09-07 17:55:28 +0530

    LC-backspace-string-compare [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/backspace-string-compare.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        string r1="",r2="";
+        for(char x : s){
+            if(x=='#')
+            r1.pop_back();
+            else{
+                r1.push_back(x);
+            }
+        }
+        for(char x : t){
+            if(x=='#')
+            r2.pop_back();
+            else{
+                r2.push_back(x);
+            }
+        }
+        return r1 == r2;
+    }
+};
