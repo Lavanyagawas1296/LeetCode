4e3c3270071b18e555519ccb4f9981c22329f97c
Lavanya Gawas <lavanyagawas07@gmail.com>
2026-09-07 18:00:01 +0530

    LC-backspace-string-compare [Runtime: N/A; Memory: N/A]

--- stack/backspace-string-compare.cpp
+++ stack/backspace-string-compare.cpp
@@ -3,15 +3,20 @@ public:
     bool backspaceCompare(string s, string t) {
         string r1="",r2="";
         for(char x : s){
-            if(x=='#' && !r1.empty())
-            r1.pop_back();
+            if(x=='#'){
+                if(!r1.empty());r1.pop_back();
+            }
+            
             else{
                 r1.push_back(x);
             }
         }
         for(char x : t){
-            if(x=='#'&& !r2.empty())
-            r2.pop_back();
+            if(x=='#'){
+                if(!r2.empty());r2.pop_back();
+
+            }
+
             else{
                 r2.push_back(x);
             }
