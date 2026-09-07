894fc5a88cf7ad13c39f46d122497d5d15f7bb1c
Lavanya Gawas <lavanyagawas07@gmail.com>
2026-09-07 18:17:45 +0530

    LC-removing-stars-from-a-string [Runtime: N/A; Memory: N/A]

---
+++ string/removing-stars-from-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string r="";
+        for(char x:s){
+            if(x=='*'){
+                if(!r.empty()) 
+                r.pop_back();
+            }
+            else{
+                r.push_back(x);
+            }
+        }
+        return r;
+    }
+};
