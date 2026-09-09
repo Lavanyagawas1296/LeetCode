67b3e398bd3ad6497713cab1cf50fd807ad92654
Lavanya Gawas <lavanyagawas07@gmail.com>
2026-09-09 23:40:57 +0530

    LC-simplify-path [Runtime: N/A; Memory: N/A]

--- stack/simplify-path.cpp
+++ stack/simplify-path.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        stack <string>st;
-        string word ="",result="";
-        for(int i =0;i<path.size();i++){
-            if(path[i]=='/'){
+        stack<string> st;
+        string word = "", result = "";
+        for (int i = 0; i < path.size(); i++) {
+            if (path[i] == '/') {
                 continue;
-            }
-            else {
-                word+=path[i];
-                if(path[i+1]=='/' || i + 1 == path.size() ){
-                    if(word==".."){
+            } else {
+                word += path[i];
+                if (path[i + 1] == '/' || i + 1 == path.size()) {
+                    if (word == "..") {
                         if (!st.empty())
-                        st.pop();
-                        word= "";
-                    }
-                    else if(word=="."){
-                        word="";
-                    }
-                    else{
+                            st.pop();
+                        
+                    } 
+                     else if (word != ".") {
                         st.push(word);
-                        word="";
+                        
                     }
+                    word = "";
                 }
             }
         }
         while (!st.empty()) {
-            result =  '/'+st.top() + result;
+            result = '/' + st.top() + result;
             st.pop();
         }
         return result.empty() ? "/" : result;
