31d82fb1a46a191578924192b65f61294929e8e7
Lavanya Gawas <lavanyagawas07@gmail.com>
2026-09-10 19:03:31 +0530

    LC-intersection-of-two-arrays [Runtime: N/A; Memory: N/A]

--- hash-table/intersection-of-two-arrays.cpp
+++ hash-table/intersection-of-two-arrays.cpp
@@ -3,8 +3,8 @@ public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         vector <int> result;
         for(int i =0 ;i<nums1.size();i++) {
-            if (std::find(nums2.begin(), nums2.end(), nums1[i]) != nums2.end() && 
-                std::find(result.begin(), result.end(), nums1[i]) == result.end()) {
+            if (find(nums2.begin(), nums2.end(), nums1[i]) != nums2.end() && 
+                find(result.begin(), result.end(), nums1[i]) == result.end()) {
                 result.push_back(nums1[i]);
             }
             
