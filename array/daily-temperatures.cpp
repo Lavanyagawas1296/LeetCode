13495eac3e9e203c6798b1161da3876ab174442e
Lavanya Gawas <lavanyagawas07@gmail.com>
2026-09-17 15:28:53 +0530

    LC-daily-temperatures [Runtime: N/A; Memory: N/A]

--- array/daily-temperatures.cpp
+++ array/daily-temperatures.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        unordered_map <int,int> hash;
-        stack<int>stk;
-        int idx=0;
-        for(int x:temperatures){
-            hash[x]=idx;
-            idx+=1;
-        }
-        for(int i=temperatures.size()-1 ; i>=0; i--){
-            if(stk.empty()){
-                stk.push(temperatures[i]);
-                temperatures[i]=0;
+        stack<int> stk;
+        vector<int> ans(temperatures.size(), 0);
+
+        for (int i = temperatures.size() - 1; i >= 0; i--) {
+
+            while (!stk.empty() && temperatures[stk.top()] <= temperatures[i]) {
+                stk.pop();
             }
-            else{
-                while(!stk.empty() && stk.top() < temperatures[i]){
-                    stk.pop();
-                }
-                if(stk.empty()){
-                    stk.push(temperatures[i]);
-                    temperatures[i]=0;
-                }
-                else{
-                    int a=stk.top();
-                    stk.push(temperatures[i]);
-                    temperatures[i]=hash[a]-i;
-                }
+
+            if (!stk.empty()) {
+                ans[i] = stk.top() - i;
             }
+
+            stk.push(i);
         }
-        return temperatures;
+
+        return ans;
     }
 };
